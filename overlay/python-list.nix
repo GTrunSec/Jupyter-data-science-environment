21766f608e765ebd313b11650cfc13a730b3de57
GTrunsec <gtrunsec@hardenedlinux.org>
2020-04-24 23:09:14 -0700

    python: update pkgs

--- overlay/python-list.nix
+++ overlay/python-list.nix
@@ -2,4 +2,5 @@
 (with pkgs.python3.withPackages; (p: with p;  [ jsondiff
                                                 geoip2
                                                 numpy pandas matplotlib editdistance ipywidgets
+                                                graphviz pillow elasticsearch requests sqlalchemy
                                      ]))
