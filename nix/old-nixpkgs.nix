52f3cd32580eb40891c0d3e1c074d20ae7508a5f
GTrunsec <gtrunsec@hardenedlinux.org>
2020-06-07 02:20:30 -0700

    remove outdata files

--- nix/old-nixpkgs.nix
+++
@@ -1,7 +0,0 @@
-let
-  src = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/db3beda6b56563777254156351b9bed349cf7d07.tar.gz";
-    sha256 = "0lkxj9pkzwxbihn3j5nyk61814m5kshpziagdzicxlns1gn1295i";
-  };
-in
-  import src
