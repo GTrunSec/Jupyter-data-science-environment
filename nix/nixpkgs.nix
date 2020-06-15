3753caa2d2c3e8f57d776a39fcbde1e1ad347b52
GTrunsec <gtrunsec@hardenedlinux.org>
2020-06-14 20:29:52 -0700

    update nixpkgs

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,7 +1,7 @@
 let
   src = builtins.fetchTarball {
-    url = "https://github.com/GTrunSec/nixpkgs/archive/4c67f879f0ee0f4eb610373e479a0a9c518c51c4.tar.gz";
-    sha256 = "1411z0df803g3pzsh5m1w4652mibwbggsza9y96w9bi7w6hrvswg";
+    url = "https://github.com/GTrunSec/nixpkgs/archive/my-release.tar.gz";
+    sha256 = "13hza44bk5fbr9zyrn9iw9z54zrwwnh7a85pxg9lb9anl3y2pxna";
   };
 in
   import src
