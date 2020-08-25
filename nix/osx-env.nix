a863222e02dc48da1b796d186193bf333c57a946
GTrunSec <gtrunsec@hardenedlinux.org>
2020-08-24 19:57:49 -0700

    test osx env

--- nix/osx-env.nix
+++ nix/osx-env.nix
@@ -1,52 +1,48 @@
 let
   jupyterLib = builtins.fetchGit {
-    url = https://github.com/tweag/jupyterWith;
-    rev = "9890afcaf982b963eb5352f633ad6531d4d6c0c7";
+    url = https://github.com/GTrunSec/jupyterWith;
+    rev = "d9fe46bce29dcdb026807335f46cb2b8655dbf6b";
+    ref = "current";
   };
 
   haskTorchSrc = builtins.fetchGit {
     url = https://github.com/hasktorch/hasktorch;
-    rev = "7e017756fd9861218bf2f804d1f7eaa4d618eb01";
+    rev = "5f905f7ac62913a09cbb214d17c94dbc64fc8c7b";
     ref = "master";
   };
 
-  hasktorchOverlay = (import (haskTorchSrc + "/nix/shared.nix") { compiler = "ghc865"; }).overlayShared;
+  hasktorchOverlay = (import (haskTorchSrc + "/nix/shared.nix") { compiler = "ghc883"; }).overlayShared;
   haskellOverlay = import ../overlay/haskell-overlay.nix;
-  env = (import (jupyterLib + "/lib/directory.nix")){ inherit pkgs;};
   overlays = [
     # Only necessary for Haskell kernel
-    (import ./overlay/python-overlay.nix)
+    (import ../overlay/python-overlay.nix)
+    (import ../overlay/package-overlay.nix)
+    (import ../overlay/julia.nix)
     haskellOverlay
     hasktorchOverlay
   ];
 
 
-  pkgs = (import (jupyterLib + "/nix/nixpkgs.nix")) { inherit overlays; config={ allowUnfree=true; allowBroken=true; };};
+  pkgs = (import ./nixpkgs.nix) { inherit overlays; config={ allowUnfree=true; allowBroken=true; };};
 
   jupyter = import jupyterLib {pkgs=pkgs;};
 
-  ihaskell_labextension = pkgs.fetchurl {
-    url = "https://github.com/GTrunSec/ihaskell_labextension/releases/download/fetchurl/package.tar.gz";
-    sha256 = "0i17yd3b9cgfkjxmv9rdv3s31aip6hxph5x70s04l9xidlvsp603";
-  };
 
   iPython = jupyter.kernels.iPythonWith {
     python3 = pkgs.callPackage ../overlay/python-self-packages.nix {};
     name = "agriculture";
-    packages = import ../overlay/python-packages-list.nix {inherit pkgs;};
+    packages = import ../overlay/osx-python-packages-list.nix {inherit pkgs;};
   };
 
   iHaskell = jupyter.kernels.iHaskellWith {
     name = "haskell";
-    haskellPackages = pkgs.haskell.packages.ghc865;
+    haskellPackages = pkgs.haskell.packages.ghc883;
     packages = p: with p; [ hvega
                             #inline-r
                             formatting
-                            libtorch-ffi_cpu
                             inline-c
                             inline-c-cpp
-                            hasktorch-examples_cpu
-                            hasktorch_cpu
+                            #hasktorch_cpu
                             matrix
                             hmatrix
                             monad-bayes
@@ -65,26 +61,42 @@ let
   };
 
 
+  iRust = jupyter.kernels.rustWith {
+    name = "data-rust-env";
+  };
+
+  IRkernel = jupyter.kernels.iRWith {
+    name = "IRkernel-data-env";
+    packages = import ../overlay/R-packages-list.nix {inherit pkgs;};
+  };
+
+  ihaskell_labextension = import ./ihaskell_labextension.nix { inherit jupyter pkgs; };
   jupyterEnvironment =
     jupyter.jupyterlabWith {
-      kernels = [ iPython iHaskell ];
-      directory = ./jupyterlab;
+      kernels = [ iPython iHaskell iRust ];
+      directory = jupyter.mkDirectoryWith {
+        extensions = [
+          ihaskell_labextension
+          "@jupyter-widgets/jupyterlab-manager@2.0"
+          "@bokeh/jupyter_bokeh@2.0.0"
+          #"@jupyterlab/git@0.21.0-alpha.0"
+          #"@krassowski/jupyterlab-lsp@1.1.2"
+        ];
+      };
     };
+
 in
 pkgs.mkShell rec {
   name = "analysis-arg";
   buildInputs = [ jupyterEnvironment
-                  env.generateDirectory
                   pkgs.python3Packages.ipywidgets
                 ];
   shellHook= ''
-    ${pkgs.python3Packages.jupyter_core}/bin/jupyter nbextension install --py widgetsnbextension --user
-    ${pkgs.python3Packages.jupyter_core}/bin/jupyter nbextension enable --py widgetsnbextension
-  if [ ! -f "./jupyterlab/extensions/ihaskell_jupyterlab-0.0.7.tgz" ]; then
-    ${env.generateDirectory}/bin/generate-directory ${ihaskell_labextension}
-  fi
-  if [ ! -f "./jupyterlab/extensions/jupyter-widgets-jupyterlab-manager-2.0.0.tgz" ]; then
-    ${env.generateDirectory}/bin/generate-directory "@jupyter-widgets/jupyterlab-manager@2.0"
-     fi
+     ${pkgs.python3Packages.jupyter_core}/bin/jupyter nbextension install --py widgetsnbextension --user
+     ${pkgs.python3Packages.jupyter_core}/bin/jupyter nbextension enable --py widgetsnbextension
+    #for emacs-ein to load kernels environment.
+      ln -sfT ${iPython.spec}/kernels/ipython_Python-data-env ~/Library/Jupyter/kernels/ipython_Python-data-env
+      ln -sfT ${iHaskell.spec}/kernels/ihaskell_ihaskell-data-env ~/Library/Jupyter/kernels/iHaskell-data-env
+      ln -sfT ${iRust.spec}/kernels/rust_data-rust-env  ~/Library/Jupyter/kernels/IRust-data-env
     '';
 }
---
+++ overlay/osx-python-packages-list.nix
@@ -0,0 +1,18 @@
+{pkgs}:
+(with pkgs.python3.withPackages; (p: with p;  [
+                                                geoip2
+                                                numpy pandas matplotlib editdistance ipywidgets
+                                                graphviz
+                                                pillow elasticsearch requests sqlalchemy
+                                                qtconsole
+                                                sympy
+                                                nbdev
+                                                # fastai2
+                                                # fastai
+                                                #zat failed by pyarrow
+                                                #financial machine learning.
+                                                #tensorflow
+                                                Keras
+                                                dask
+                                                pyfolio
+                                              ]))
