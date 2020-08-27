eacbe7ab06e7c4e70698d83ceaf6f318672b7971
GTrunSec <gtrunsec@hardenedlinux.org>
2020-08-26 18:33:32 -0700

    update Julia1.4.2 to 1.5.1

--- overlay/julia.nix
+++ overlay/julia.nix
@@ -2,8 +2,8 @@ self: super:
 {
   julia_13 = super.julia_13.overrideAttrs(oldAttrs: {
    src = super.fetchzip {
-     url = "https://github.com/JuliaLang/julia/releases/download/v1.4.2/julia-1.4.2-full.tar.gz";
-     sha256 = "14jghi9mw0wdi6y9saarf0nzary9i21jx43zznddzrq48v4nlayj";
+     url = "https://github.com/JuliaLang/julia/releases/download/v1.5.1/julia-1.5.1-full.tar.gz";
+     sha256 = "sha256-uaxlzni2RtmDhMzPbtDycj44CB0tJUzhmbwsAGwFv/U=";
    };
    checkTarget = "";
   });
