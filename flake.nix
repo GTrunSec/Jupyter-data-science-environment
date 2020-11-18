68d655425dd5a9566b9c6465fcf2714c38e888ab
GTrunSec <gtrunsec@hardenedlinux.org>
2020-11-18 05:34:55 -0800

    update python37 test

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "nixpkgs/703f052de185c3dd1218165e62b105a68e05e15f";
+    python37.url = "nixpkgs/4c67f879f0ee0f4eb610373e479a0a9c518c51c4"; #python3.7 tensorflow_2
     nixpkgs-hardenedlinux = { url = "github:hardenedlinux/nixpkgs-hardenedlinux/master"; flake = false; };
     haskTorch = { url = "github:hasktorch/hasktorch/5f905f7ac62913a09cbb214d17c94dbc64fc8c7b"; flake = false; };
     jupyterWith = { url = "github:GTrunSec/jupyterWith/Nov"; flake = false; };
--- overlays/python-packages-list.nix
+++ overlays/python-packages-list.nix
@@ -13,30 +13,30 @@
                sympy
                #nbdev
              ]  ++ pkgs.lib.optionals DataScience [
-                 numpy
-                 pandas
-                 # fastai2
-                 # fastai
+               numpy
+               pandas
+               # fastai2
+               # fastai
              ] ++ pkgs.lib.optionals Graph [
-                 #editdistance
-                 ipywidgets
-                 graphviz
-                 geoip2
-                 pillow
-                 matplotlib
+               #editdistance
+               ipywidgets
+               graphviz
+               geoip2
+               pillow
+               matplotlib
              ] ++ pkgs.lib.optionals MachineLearning [
-                 #tensorflow
-                 Keras
-                 pytorch-bin
+               (if pkgs.python.passthru.isPy38 then tensorflow_2 else "")
+               Keras
+               (if pkgs.python.passthru.isPy38 then pytorch_bin else pytorch)
              ]  ++ pkgs.lib.optionals Financial [
-                 #financial machine learning.
-                 #mlfinlab
-                 tqdm
-                 patsy
-                 dask
-                 #pyfolio
+               #financial machine learning.
+               #mlfinlab
+               tqdm
+               patsy
+               dask
+               #pyfolio
              ] ++ pkgs.lib.optionals SecurityAnalysis [
-                 #zat
+               #zat
              ]
 
 )
