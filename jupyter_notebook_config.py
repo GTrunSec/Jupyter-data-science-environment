3eb81aea1cbee43272f0963c5cc6fcda6e0b55f0
GTrunSec <gtrunsec@hardenedlinux.org>
2020-11-18 21:09:24 -0800

    update jupytext conf

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python3.7
 c.NotebookApp.contents_manager_class = "jupytext.TextFileContentsManager"
-c.ContentsManager.notebook_extensions = "ipynb,Rmd,jl,lhs"
+c.ContentsManager.notebook_extensions = "ipynb,Rmd,jl,md,py"
